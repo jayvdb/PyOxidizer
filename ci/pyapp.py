b54aeeb1519bd2ff9f287ff4ae8a8a090a623f59
John Vandenberg <jayvdb@gmail.com>
2019-11-19 08:55:21 +0700

    Unix CI: Verify building cryptography

---
+++ ci/pyapp.py
@@ -0,0 +1,15 @@
+import cryptography.fernet
+
+m = b'does this work?'
+s = 'original message:\n' + str(m) + '\n\n'
+
+k = cryptography.fernet.Fernet.generate_key()
+fernet = cryptography.fernet.Fernet(k)
+t = fernet.encrypt(m)
+d = fernet.decrypt(t)
+
+s += 'key:\n' + str(k) + '\n\n'
+s += 'token:\n' + str(t) + '\n\n'
+s += 'decoded message:\n' + str(d)
+print(s)
+assert d == m
