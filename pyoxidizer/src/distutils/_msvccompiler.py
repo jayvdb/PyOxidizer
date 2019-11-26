ef8c51705e88bfb7891d1d79a3e1141b2e6f6a09
John Vandenberg <jayvdb@gmail.com>
2019-11-26 23:50:02 +0700

    Fix greenlet

--- pyoxidizer/src/distutils/_msvccompiler.py
+++ pyoxidizer/src/distutils/_msvccompiler.py
@@ -593,7 +593,11 @@ class MSVCCompiler(CCompiler) :
                 # https://github.com/indygreg/python-build-standalone/issues/23
                 # Library 'ffi' should be added here
                 continue
-            o = o + '.static'
+            # If there are pre-compiled .obj files provided
+            # they will not have a .static version, and the
+            # original should be used.  greenlet does this.
+            if os.path.exists(o + '.static'):
+                o = o + '.static'
             p = os.path.join(dest_path, '%s.%d.o' % (name, i))
             shutil.copyfile(o, p)
             object_paths.append(p)
